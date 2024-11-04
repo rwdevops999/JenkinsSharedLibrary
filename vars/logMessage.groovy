760df8578eb70b3bdc746b88d9d8d73b6b61eb74
Rudi Welter <rudi.welter.devops@gmail.com>
2024-11-04 18:45:23 +0100

    Update logMessage.groovy

--- vars/logMessage.groovy
+++ vars/logMessage.groovy
@@ -1,6 +1,7 @@
+import java.text.SimpleDateFormat
+
 def call (String message) {
-//    def date = new Date()
-//    def sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
-//    println (sdf.format(date) + " : ${message}")
-    println message
+    def date = new Date()
+    def sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
+    println (sdf.format(date) + " : ${message}")
 }
